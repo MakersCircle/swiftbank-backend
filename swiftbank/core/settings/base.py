903307be3ba7f2ff25296490aa906d5ab3333de0
don-sabu <donsabu@tutanota.com>
2024-08-16 01:15:31 +0530

    Added docker cmpose and health check

--- swiftbank/core/settings/base.py
+++ swiftbank/core/settings/base.py
@@ -7,7 +7,7 @@ from decouple import config as decouple_config
 SECRET_KEY = decouple_config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = decouple_config('DEBUG')
+DEBUG = decouple_config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [decouple_config('ALLOWED_HOSTS')]
 
